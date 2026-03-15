d9e85106f3ebd6c8bbad4e43aea1a4581f4e785c
nnam09 <khachhanhtltd663@gmail.com>
2026-03-15 03:46:06 +0000

    Create solve.py

---
+++ Forensics/Cold Workspace /solve.py
@@ -0,0 +1,21 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import base64
+
+# Extracted from your strings output
+encd_b64 = "S4wX8ml7/f9C2ffc8vENqtWw8Bko1RAhCwLLG4vvjeT2iJ26nfeMzWEyx/HlK1KmOhIrSMoWtmgu2OKMtTtUXddZDQ87FTEXIqghzCL6ErnC1+GwpSfzCDr9woKXj5IzcU2C/Ft5u705bY3b6/Z/Q/N6MPLXV55pLzIDnO1nvtja123WWwH54O4mnyWNspt5"
+enck_b64 = "Ddf4BCsshqFHJxXPr5X6MLPOGtITAmXK3drAqeZoFBU="
+encv_b64 = "xXpGwuoqihg/QHFTM2yMxA=="
+
+ciphertext = base64.b64decode(encd_b64)
+key = base64.b64decode(enck_b64)
+iv = base64.b64decode(encv_b64)
+
+cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+decryptor = cipher.decryptor()
+plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+
+with open("flag_recovered.jpg", "wb") as f:
+    f.write(plaintext)
+
+print("Decryption complete! Check flag_recovered.jpg")
